cce23804e525efdf4cd52938fa930b49e42d937f
Elad Alon <elad.alon@gmail.com>
2022-03-03 11:34:33 +0100

    Add .js file for actions

---
+++ actions/capture-handle-actions.js
@@ -0,0 +1,13 @@
+var respecConfig = {
+  group: "webrtc",
+  specStatus: "unofficial",
+  github: {
+    repoURL: "https://github.com/wicg/capture-handle/actions/",
+    branch: "master",
+  },
+  editors: [
+  ],
+  xref: ["html", "infra", "permissions", "permissions-policy", "dom", "mediacapture-streams", "webidl", "screen-capture"],
+  shortName: "capture-handle-actions",
+  subjectPrefix: "[capture-handle-actions]",
+};
